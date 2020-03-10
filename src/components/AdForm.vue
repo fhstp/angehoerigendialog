933addd41575b63f17fffb8de6f84b30d2ca6591
Babsi <mt171013@fhstp.ac.at>
2020-03-10 17:53:16 +0100

    add first question type and scaffold for form

--- src/components/AdForm.vue
+++ src/components/AdForm.vue
@@ -1,9 +1,60 @@
 <template>
-  <div />
+  <div>
+    <section
+      v-for="(section, sectionId) in form"
+      :key="sectionId"
+    >
+      <h2>{{ section.title }}</h2>
+
+      <div
+        v-for="(field, fieldId) in section.fields"
+        :key="fieldId"
+        class="question"
+      >
+        <component
+          :is="mapping['text']"
+          v-if="field.type === 'text'"
+          :field-id="`${sectionId}-${fieldId}`"
+          :field-label="field.label"
+          :field-required="field.required"
+        />
+        <div v-else>
+          Frage von Typ: {{ field.type }}
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
+import form from '@/data/form.json'
+
+// Question Types
+import AdText from './fields/AdText';
+
 export default {
-  name: 'AdForm'
+  name: 'AdForm',
+  components: {
+    AdText
+  },
+  created() {
+    this.mapping = {
+      text: 'AdText'
+    };
+
+    this.form = form
+  }
 };
 </script>
+
+<style lang="scss" scoped>
+section:nth-child(odd) {
+  background-color: crimson;
+}
+.question {
+  margin: 1rem;
+}
+.question:nth-child(odd) {
+  background-color: greenyellow
+}
+</style>
---
+++ src/components/fields/AdText.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <label :for="fieldId">{{ fieldLabel }}</label>
+    <textarea
+      :id="fieldId"
+      :required="fieldRequired"
+      cols="50"
+      rows="3"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AdText',
+  props: {
+    fieldId: String,
+    fieldLabel: String,
+    fieldRequired: Boolean
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
