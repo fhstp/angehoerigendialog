b4ca1d66ff6d123ee166bb456b62691d6131029a
Babsi <mt171013@fhstp.ac.at>
2020-03-10 17:06:45 +0100

    adds build process for form yaml

---
+++ data/_converter.js
@@ -0,0 +1,14 @@
+const YAML = require('yaml');
+const fs = require('fs');
+
+const contentYaml = fs.readFileSync('./data/_inhaltsverzeichnis.yml', 'utf8');
+const content = YAML.parse(contentYaml);
+
+const form = {};
+
+for (const section of content) {
+  const sectionYaml = fs.readFileSync(`./data/${section}.yml`, 'utf8');
+  form[section] = YAML.parse(sectionYaml);
+}
+
+fs.writeFileSync('./src/data/form.json', JSON.stringify(form), 'utf8');
