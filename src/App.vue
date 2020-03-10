c12209012b9456cbf1104ed1e110933d40ed891a
Jan Kollars <kollars.jan@gmail.com>
2020-03-10 11:09:01 +0100

    project primer
    - sets up pre commit hook
    - sets up linting/formatting
    - moves configs to package.json
    - removes dummy files
    - adds components establishing naming convention
    - sets up SCSS

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,22 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <AdForm />
+    <AdVisualisation />
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import AdForm from '@/components/AdForm.vue';
+import AdVisualisation from '@/components/AdVisualisation.vue';
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+export default {
+  components: {
+    AdForm,
+    AdVisualisation
   }
-}
+};
+</script>
+
+<style lang="scss">
+@import '@/assets/styles/base.scss';
 </style>
