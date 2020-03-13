4ca46db3c6cb2c8bdbbabc07fb3d667219e4135e
Jan Kollars <kollars.jan@gmail.com>
2020-03-13 12:43:14 +0100

    adds state for answers and initiate it

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,27 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import form from '@/data/form.json';
 
 Vue.use(Vuex);
 
+for (const section in form) {
+  form[section] = {};
+}
+
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  state: {
+    answers: {}
+  },
+  mutations: {
+    initAnswers(state) {
+      state.answers = form;
+    }
+  },
+  actions: {
+    resetForm({commit}) {
+      commit('initAnswers');
+    }
+  },
+  strict: process.env.NODE_ENV !== 'production'
+  // https://vuex.vuejs.org/guide/getters.html#method-style-access
 });
