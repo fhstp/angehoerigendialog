9b56afb434407b6e24291f736666ba46780ea320
Jan Kollars <kollars.jan@gmail.com>
2020-03-16 15:34:28 +0100

    improves form data
    - sets `required` to true by default
    - use _ to combine multi word keys
    
    Co-authored-by: Barbara Ehrenhöfer's avatarbabsi0405 <mt171013@fhstp.ac.at>
    Co-authored-by: Felix De Montis's avatarFelix De Montis <felix@demont.is>
    Co-authored-by: Johannes Gerstbauer's avatarJohannes-Webdev <johannes.gerstbauer@gmail.com>

--- src/mixins/field.js
+++ src/mixins/field.js
@@ -3,7 +3,7 @@ export default {
     field_type: {type: String, required: true},
     field_id: {type: String, default: undefined},
     field_label: {type: String, default: undefined},
-    field_required: {type: Boolean, default: false},
+    field_required: {type: Boolean, default: true},
     field_when: {type: Object, default: undefined}
   },
   computed: {
