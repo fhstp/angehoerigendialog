71d088239f6692a7fb186e4a821c8dbb35d04efc
Babsi <mt171013@fhstp.ac.at>
2020-03-17 10:41:28 +0100

    adds first version of Heading Field

---
+++ src/components/fields/AnHeading.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="an-text">
+    <h3 :for="field_id">
+      {{ field_label }}
+    </h3>
+  </div>
+</template>
+
+<script>
+import field from '@/mixins/field';
+
+export default {
+  name: 'AnHeading',
+  mixins: [field]
+};
+</script>
--- src/components/fields/index.js
+++ src/components/fields/index.js
@@ -1,5 +1,7 @@
 import AnText from './AnText.vue';
+import AnHeading from './AnHeading.vue';
 
 export {
+  AnHeading,
   AnText
 };
