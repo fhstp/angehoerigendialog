a29e046afea6bcf2e22aa98a5dbef7bceafbc05f
Johannes Gerstbauer <mt171108@fhstp.ac.at>
2020-03-24 17:42:48 +0100

    adds date, time and submit components

---
+++ src/components/fields/AnDate.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <label :for="field_id" class="field__question">{{ field_label }}</label>
+    <input :id="field_id" v-model="field_data" type="date" required />
+  </div>
+</template>
+
+<script>
+import field from '@/mixins/field.js';
+
+export default {
+  name: 'AnDate',
+  mixins: [field],
+  mounted() {
+    const date = new Date();
+    const today = `${date.getFullYear()}-${this.formatDate(
+      date.getMonth() + 1
+    )}-${this.formatDate(date.getDate())}`;
+    this.field_data = today;
+  },
+  methods: {
+    formatDate: value => {
+      value = String(value);
+      if (value.length === 2) {
+        return value;
+      }
+      return '0' + value;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/fields/AnTime.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <label :for="field_id" class="field__question">{{ field_label }}</label>
+    <input :id="field_id" v-model="field_data" type="time" required />
+  </div>
+</template>
+
+<script>
+import field from '@/mixins/field.js';
+
+export default {
+  name: 'AnTime',
+  mixins: [field],
+  mounted() {
+    const today = new Date();
+    const time = `${this.formatTime(today.getHours())}:${this.formatTime(
+      today.getMinutes()
+    )}`;
+    this.field_data = time;
+  },
+  methods: {
+    formatTime: value => {
+      value = String(value);
+      if (value.length === 2) {
+        return value;
+      }
+      return '0' + value;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
