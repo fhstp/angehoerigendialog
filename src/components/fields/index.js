a359b3a94b6f08b06b5fb219639937820043ea30
Felix De Montis <felix@demont.is>
2020-03-26 11:00:03 +0100

    replaces normal checkboxes with megacheckboxes
    checkboxes have now all the features of megacheckboxes
    
    Co-authored-by: babsi0405 <mt171013@fhstp.ac.at>
    Co-authored-by: kollars.jan@outlook.com <kollars.jan@outlook.com>
    Co-authored-by: Johannes-Webdev <johannes.gerstbauer@gmail.com>

--- src/components/fields/index.js
+++ src/components/fields/index.js
@@ -2,7 +2,6 @@ import AnCheckboxes from './AnCheckboxes.vue';
 import AnHeading from './AnHeading.vue';
 import AnHint from './AnHint.vue';
 import AnList from './AnList.vue';
-import AnMegacheckboxes from './AnMegacheckboxes.vue';
 import AnNumber from './AnNumber.vue';
 import AnRadio from './AnRadio.vue';
 import AnRange from './AnRange.vue';
@@ -14,7 +13,6 @@ export {
   AnHeading,
   AnHint,
   AnList,
-  AnMegacheckboxes,
   AnNumber,
   AnRadio,
   AnRange,
