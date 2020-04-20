83d1803c67e8b1348f7f838359f9b148e6be0d49
Jan Kollars <kollars.jan@gmail.com>
2020-04-20 12:11:04 +0200

    ADBP-32 creates resources visualisation
    WIP: as no content is provided

---
+++ src/components/visualisations/AnResources.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="an-resources row">
+    <div class="an-resources__icon-wrapper col-md-3">
+      <IconBeach class="an-resources__icon" />
+    </div>
+    <div class="col-md-3">
+      <ul>
+        <li v-for="(resource, i) in resources" :key="i">{{ resource }}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import IconBeach from '@/assets/icons/beach.svg';
+
+export default {
+  name: 'AnResources',
+  components: { IconBeach },
+  computed: {
+    resources() {
+      return [];
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.an-resources {
+  &__icon-wrapper {
+    display: flex;
+    align-content: center;
+    justify-content: center;
+  }
+
+  &__icon {
+    width: 75%;
+  }
+}
+</style>
