d4b4835af31b037db36f0c2dee914f8903a634f6
Jan Kollars <kollars.jan@gmail.com>
2020-04-23 11:25:51 +0200

    ADBP-29 adds visualisation for current Situation
    WIP: data

---
+++ src/components/visualisations/AnSituation.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="an-situation row">
+    <div class="col-md-3">
+      <IconMood class="an-situation__icon" />
+      <ul>
+        <li v-for="(item, i) in aspectsPositive" :key="i">{{ item }}</li>
+      </ul>
+    </div>
+    <div class="col-md-3">
+      <IconMoodBad class="an-situation__icon" />
+      <ul>
+        <li v-for="(item, i) in aspectsNegative" :key="i">{{ item }}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import IconMood from '@/assets/icons/mood.svg';
+import IconMoodBad from '@/assets/icons/mood_bad.svg';
+
+export default {
+  name: 'AnSituation',
+  components: { IconMood, IconMoodBad },
+  computed: {
+    aspectsPositive() {
+      return [];
+    },
+    aspectsNegative() {
+      return [];
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.an-situation {
+  &__icon {
+    display: block;
+    width: 100%;
+    max-width: 200px;
+    margin: 0 auto;
+  }
+}
+</style>
--- src/views/Visualisation.vue
+++ src/views/Visualisation.vue
@@ -3,16 +3,20 @@
     <h1>Auswertung</h1>
     <h2>Meine Ressourcen</h2>
     <AnResources />
+    <h2>Zusammenfassende Einschätzung der gesundheitliche Situation</h2>
+    <AnSituation />
   </div>
 </template>
 
 <script>
 import AnResources from '@/components/visualisations/AnResources.vue';
+import AnSituation from '@/components/visualisations/AnSituation.vue';
 
 export default {
   name: 'Visualisation',
   components: {
-    AnResources
+    AnResources,
+    AnSituation
   }
 };
 </script>
