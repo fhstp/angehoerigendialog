e068fe417f4cbf7893e29e4049f093e546f3fd20
Jan Kollars <kollars.jan@gmail.com>
2020-04-29 14:38:05 +0200

    adds thumb label to range input, makes range direction consistent

--- src/components/fields/AnRange.vue
+++ src/components/fields/AnRange.vue
@@ -1,20 +1,24 @@
 <template>
   <div class="an-range">
     <div class="an-range_wrapper">
-      <span class="an-range_min-value">
-        {{ minDescription }}
+      <span class="an-range_min-label">
+        {{ fieldMin.label }}
       </span>
-      <input
-        :id="field_id"
-        v-model.number="field_data"
-        type="range"
-        :required="field_required"
-        :min="minValue"
-        :max="maxValue"
-        :step="fieldStep"
-      />
-      <span class="an-range_max-value">
-        {{ maxDescription }}
+      <div class="an-range__input-area" :style="{ '--value': valueAsPercent }">
+        <div class="an-range__thumb-label">{{ field_data }}</div>
+        <input
+          :id="field_id"
+          v-model.number="field_data"
+          class="an-range__slider"
+          type="range"
+          :required="field_required"
+          :min="fieldMin.value"
+          :max="fieldMax.value"
+          :step="fieldStep"
+        />
+      </div>
+      <span class="an-range_max-label">
+        {{ fieldMax.label }}
       </span>
     </div>
   </div>
@@ -32,21 +36,47 @@ export default {
     fieldMax: { type: Array, required: true }
   },
   computed: {
-    minValue() {
-      return this.fieldMin[0];
-    },
-    minDescription() {
-      return this.fieldMin[1];
-    },
-    maxValue() {
-      return this.fieldMax[0];
-    },
-    maxDescription() {
-      return this.fieldMax[1];
+    valueAsPercent() {
+      return (
+        (this.field_data - this.fieldMin.value) /
+        (this.fieldMax.value - this.fieldMin.value)
+      );
     }
-  },
-  created() {
-    if (!this.field_data) this.field_data = this.minValue;
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.an-range {
+  &__input-area {
+    display: inline-block;
+    position: relative;
+    margin: 0 $spacer * 2;
+  }
+
+  &__thumb-label {
+    position: absolute;
+    top: -5px;
+    left: calc(100% * var(--value));
+    transform: translate(-50%, -100%);
+    border-radius: 3px;
+    background-color: #333;
+    color: white;
+    padding: $spacer / 2;
+    min-width: 3ch;
+    text-align: center;
+    line-height: 1;
+
+    &::after {
+      content: '';
+      position: absolute;
+      bottom: -5px;
+      left: 50%;
+      transform: translateX(-50%);
+      border-top: 5px solid #333;
+      border-right: 5px solid transparent;
+      border-left: 5px solid transparent;
+    }
+  }
+}
+</style>
