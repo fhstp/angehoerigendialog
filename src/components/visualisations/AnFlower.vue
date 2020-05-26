f858976f7193df744949f5e9a0e151e218d3c903
Felix De Montis <felix@demont.is>
2020-05-26 11:46:43 +0200

    Fixes Corner and SVG loading issues

--- src/components/visualisations/AnFlower.vue
+++ src/components/visualisations/AnFlower.vue
@@ -14,27 +14,21 @@
         ></div>
         <div class="an-flower__text" v-html="answer.text"></div>
       </div>
+      <div
+        class="an-flower__overlay"
+        :class="getClass(answers[0].type, 0)"
+      ></div>
+      <div
+        class="an-flower__overlay_line"
+        :class="getClass(answers[1].type, 0)"
+      ></div>
     </div>
     <div v-if="wateringcanItems.length" class="an-flower__wateringcan">
       <div class="an-flower__wateringcan__circle"></div>
-      <!-- TODO: include via component, doesn't work because viewBox gets lost -->
-      <!-- <Iconwateringcan
-        class="wateringcan__trunk"
-        :style="{ '--stroke': -0.4 * wateringcanItems.length + 3.4 + 'px' }"
-      /> -->
-      <svg
+      <IconWateringcan
         class="an-flower__wateringcan__trunk"
         :style="{ '--stroke': -0.4 * wateringcanItems.length + 3.4 + 'px' }"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 100 100"
-        preserveAspectRatio="none"
-        width="100"
-        height="100"
-      >
-        <path
-          d="M16.63,1,99,70.46V97.59L2.41,1H16.63M17,0H0L100,100V70L17,0Z"
-        />
-      </svg>
+      />
       <div class="an-flower__wateringcan__wrapper">
         <div class="an-flower__wateringcan__innerbox">
           <ul class="an-flower__wateringcan__list">
@@ -54,10 +48,10 @@
 <script>
 import visJson from '@/data/visualisation.json';
 import visualisation from '@/mixins/visualisation.js';
-// import Iconwateringcan from '@/assets/icons/wateringcan.svg?inline';
+import IconWateringcan from '@/assets/icons/wateringcan.svg?inline';
 export default {
   name: 'AnFlower',
-  // components: { Iconwateringcan },
+  components: { IconWateringcan },
   mixins: [visualisation],
   computed: {
     answers() {
@@ -101,7 +95,7 @@ $centerSize: 80px;
 $centerSizeHalf: $centerSize / 2;
 
 .an-flower {
-  -webkit-print-color-adjust: exact;
+  color-adjust: exact;
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -122,6 +116,54 @@ $centerSizeHalf: $centerSize / 2;
     top: $size - $centerSizeHalf;
     left: $size - $centerSizeHalf;
     transform: translate(-50%, -50%);
+    z-index: 3;
+  }
+
+  &__overlay {
+    content: '';
+    height: $centerSize / 4 * 3;
+    width: $centerSize / 8 * 7;
+    position: absolute;
+
+    &.one {
+      background-color: $red;
+    }
+
+    &.two,
+    &.three {
+      background-color: $blue;
+    }
+
+    z-index: 1;
+    top: $size - $centerSize;
+    left: $size - $centerSize;
+    transform-origin: $centerSizeHalf $centerSizeHalf;
+    transform: rotate(-60deg) translateX($centerSizeHalf);
+    border-top: 6px solid;
+    border-color: $background;
+  }
+
+  &__overlay_line {
+    height: $centerSize / 4;
+    position: absolute;
+    width: $centerSize / 8 * 5;
+    border-top: 6px solid;
+    border-color: $background;
+
+    &.one {
+      background-color: $red;
+    }
+
+    &.two,
+    &.three {
+      background-color: $blue;
+    }
+
+    transform-origin: $centerSizeHalf $centerSizeHalf;
+    transform: translateX($centerSizeHalf);
+    z-index: 2;
+    top: $size - $centerSize;
+    left: $size - $centerSize;
   }
 
   &__pillwrapper {
