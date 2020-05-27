532a2a8570ee40d8512a5b6c4b94387165ba26e4
Babsi <mt171013@fhstp.ac.at>
2020-05-27 12:38:24 +0200

    gets data

--- src/components/visualisations/AnBasisInformation.vue
+++ src/components/visualisations/AnBasisInformation.vue
@@ -1,122 +1,71 @@
 <template>
   <div class="an-basisinformation row">
-    <button
-      :class="[
-        'col-md-3',
-        'btn',
-        { 'btn--active': btnActive_betreuendePerson || printMode }
-      ]"
-      @click="btnActive_betreuendePerson = !btnActive_betreuendePerson"
-    >
-      Betreuende Person
-    </button>
-
-    <button
-      :class="[
-        'col-md-3',
-        'btn',
-        { 'btn--active': btnActive_demenzPerson || printMode }
-      ]"
-      @click="btnActive_demenzPerson = !btnActive_demenzPerson"
-    >
-      Demenzerkrankte Person
-    </button>
-
-    <table
-      v-if="btnActive_betreuendePerson || printMode"
-      class="an-basisinformation__table col-md-3"
-    >
-      <tr v-for="(answer, row) in answerArray_betreuendePerson[0]" :key="row">
-        <td>{{ tdHeading['betreuende_person'][row] }}</td>
-        <td :colspan="answer === 'ja' && row === 1 ? 0 : 2">{{ answer }}</td>
-        <td v-if="answer === 'ja' && row === 1">
-          {{ answerArray_betreuendePerson[1][0] }}h
-        </td>
-      </tr>
-    </table>
-
-    <table
-      v-if="btnActive_demenzPerson || printMode"
-      class="an-basisinformation__table col-md-3"
-    >
-      <tr v-for="(answer, row) in answerArray_demenzPerson" :key="row">
-        <td>{{ tdHeading['demenzerkrankte_person'][row] }}</td>
-        <td>{{ answer }}</td>
-      </tr>
-    </table>
+    <div class="col-md-2">
+      <h3>Betreuende Person</h3>
+      <div v-for="(info, i) in infoBetreuendePerson" :key="i">
+        {{ info.label }}
+        {{ info.text }}
+        {{ info.value }}
+      </div>
+    </div>
+    <div class="col-md-2">
+      <h3>Demenzerkrankte Person</h3>
+      <div v-for="(info, i) in infoDemenzPerson" :key="i">
+        {{ info.label }}
+        {{ info.text }}
+        {{ info.value }}
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import visJson from '@/data/visualisation.json';
+
 export default {
   name: 'AnBasisinformation',
-  data() {
-    return {
-      btnActive_betreuendePerson: false,
-      btnActive_demenzPerson: false,
-      tdHeading: {
-        betreuende_person: [
-          'Alter',
-          'Berufstätigkeit',
-          'Weitere Betreuungs- oder Sorgepflichten',
-          'Unterstützungsangebote'
-        ],
-        demenzerkrankte_person: [
-          'Verwandtschaftliches Verhältnis',
-          'Alter',
-          'Unterstüztungsbedarf seit',
-          'Ärztliche Demenzdiagnose',
-          'Pflegestufe',
-          'Gemeinsamer Haushalt',
-          'Unterstützungsangebote'
-        ]
-      },
-      fieldKey: {
-        betreuende_person: [
-          'alter',
-          'beruf',
-          'berufdetail',
-          'sorgepflichten',
-          'unterstuetzungsangebote'
-        ],
-        demenzerkrankte_person: [
-          'verhaeltnis',
-          'alter',
-          'pflegebedarf',
-          'diagnose',
-          'pflegegelddetail',
-          'haushalt',
-          'unterstuetzungsangebot'
-        ]
-      },
-      answerArray_betreuendePerson: [[], []],
-      answerArray_demenzPerson: []
-    };
-  },
   computed: {
-    printMode() {
-      return this.$store.state.printMode;
-    }
-  },
-  mounted() {
-    this.getBasisinfo('betreuende_person');
-    this.getBasisinfo('demenzerkrankte_person');
-  },
-  methods: {
-    getBasisinfo(sectionKey) {
-      this.fieldKey[sectionKey].forEach(key => {
-        let fieldValue = this.$store.getters.getFieldValue(
-          `${sectionKey}-${key}`
+    infoBetreuendePerson() {
+      const basisinfoLabel = visJson.visualisation.basisinfo.betreuendeperson;
+      const basisInfoValues = [];
+
+      Object.keys(basisinfoLabel).forEach(item => {
+        const value = this.$store.getters.getFieldValue(
+          `betreuende_person-${item}`
         );
-        if (fieldValue === undefined) fieldValue = '-';
-        if (fieldValue === false) fieldValue = 'nein';
-        else if (fieldValue === true) fieldValue = 'ja';
-        if (sectionKey === 'betreuende_person') {
-          if (key === 'berufdetail') {
-            this.answerArray_betreuendePerson[1].push(fieldValue);
-          } else this.answerArray_betreuendePerson[0].push(fieldValue);
-        } else this.answerArray_demenzPerson.push(fieldValue);
+
+        if (value) {
+          basisInfoValues.push({
+            label: basisinfoLabel[item],
+            text: 'Ja',
+            value: this.$store.getters.getFieldValue(
+              `betreuende_person-${item}detail`
+            )
+          });
+        }
       });
+      return basisInfoValues;
+    },
+    infoDemenzPerson() {
+      const basisinfoLabel = visJson.visualisation.basisinfo.demenzperson;
+      const basisInfoValues = [];
+
+      Object.keys(basisinfoLabel).forEach(item => {
+        const value = this.$store.getters.getFieldValue(
+          `demenzerkrankte_person-${item}`
+        );
+
+        if (value) {
+          basisInfoValues.push({
+            label: basisinfoLabel[item],
+            text: 'Ja',
+            value: this.$store.getters.getFieldValue(
+              `demenzerkrankte_person-${item}detail`
+            )
+          });
+        }
+      });
+      return basisInfoValues;
     }
   }
 };
