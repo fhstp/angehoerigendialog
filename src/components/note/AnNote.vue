e805f5dd2727efbdabe7d0638b4ea4af9d414565
doomsayer2 <fgrassinger@gmx.at>
2020-11-25 16:14:13 +0100

    Remove html tags from notes (refs #27).

--- src/components/note/AnNote.vue
+++ src/components/note/AnNote.vue
@@ -150,7 +150,11 @@ export default {
     addHeading() {
       const textarea_alreadythere = this.$refs.ta_alreadythere;
       const textarea_newtext = this.$refs.ta_newtext;
-      const heading_inserted = `**${this.currentQuestionLabel}**`;
+      const cleanedLabel = this.currentQuestionLabel.replace(
+        /(<([^>]+)>)/gi,
+        ''
+      );
+      const heading_inserted = `**${cleanedLabel}**`;
       this.currentQuestionLabel_prev = this.currentQuestionLabel;
 
       if (textarea_alreadythere.value !== '') {
